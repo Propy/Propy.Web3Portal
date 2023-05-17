e04eacd7e1c3ac30cc284b4f3d2b46f0320d3dcc
Jay Welsh <thealchemicalopus@gmail.com>
2023-05-17 18:19:54 +0200

    feat(GenericBannerPage): responsiveness

--- src/components/GenericBannerPage.tsx
+++ src/components/GenericBannerPage.tsx
@@ -23,6 +23,17 @@ const useStyles = makeStyles((theme: Theme) =>
       position: 'relative',
       top: -150,
       margin: theme.spacing(8),
+    },
+    childContainerLarge: {
+      margin: theme.spacing(8),
+      marginTop: 0,
+    },
+    childContainerMedium: {
+      margin: theme.spacing(4),
+      marginTop: 0,
+    },
+    childContainerMobile: {
+      margin: theme.spacing(2),
       marginTop: 0,
     },
     titleContainer: {
@@ -52,10 +63,22 @@ const GenericBannerPage = (props: PropsFromRedux & IGenericBannerPage) => {
     isConsideredMedium,
   } = props;
 
+  const getChildContainerClass = (isMobile: boolean, isMedium: boolean) => {
+    let childClasses = [`${classes.childrenContainer}`];
+    if(isMobile) {
+      childClasses.push(classes.childContainerMobile);
+    } else if(isMedium) {
+      childClasses.push(classes.childContainerMedium);
+    } else {
+      childClasses.push(classes.childContainerLarge);
+    }
+    return childClasses.join(" ");
+  }
+
   return (
     <div className={classes.root}>
       <div className={classes.bannerImage} style={{backgroundImage: `url("${img}")`}} />
-      <div className={classes.childrenContainer}>
+      <div className={getChildContainerClass(isConsideredMobile, isConsideredMedium)}>
         <div className={classes.titleContainer}>
           <Typography variant="h2" component="h2" className={["text-shadow", classes.title].join(" ")}>
             {title}
