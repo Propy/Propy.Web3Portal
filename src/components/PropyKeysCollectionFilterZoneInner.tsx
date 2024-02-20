8a92af24b31385c5dc4932d7fb05d5b631df0c42
Jay Welsh <thealchemicalopus@gmail.com>
2024-02-20 16:11:46 +0200

    chore(PropyKeysCollectionFilterZoneInner): mobile improvement

--- src/components/PropyKeysCollectionFilterZoneInner.tsx
+++ src/components/PropyKeysCollectionFilterZoneInner.tsx
@@ -154,7 +154,7 @@ const PropyKeysCollectionFilterZoneInner = (props: ICollectionFilterZone) => {
               <Autocomplete
                 id="country-filter"
                 options={uniqueCountries}
-                sx={{ width: 300 }}
+                sx={{ maxWidth: 300 }}
                 className={classes.inputSpacerSmall}
                 renderInput={(params) => <TextField {...params} label="Country" />}
                 onChange={(event, value, reason, details) => {
@@ -170,7 +170,7 @@ const PropyKeysCollectionFilterZoneInner = (props: ICollectionFilterZone) => {
               <Autocomplete
                 id="city-filter"
                 options={uniqueCities}
-                sx={{ width: 300 }}
+                sx={{ maxWidth: 300 }}
                 className={classes.inputSpacer}
                 onChange={(event, value, reason, details) => {
                   if(value) {
