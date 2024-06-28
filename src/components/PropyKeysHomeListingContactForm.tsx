d319b4588fb65648b160e37ee8d417bb0ef1be9f
Jay Welsh <thealchemicalopus@gmail.com>
2024-06-28 23:12:37 +0200

    fix(PropyKeysHomeListingContactForm): avoid early call to recaptcha

--- src/components/PropyKeysHomeListingContactForm.tsx
+++ src/components/PropyKeysHomeListingContactForm.tsx
@@ -94,27 +94,9 @@ const PropyKeysHomeListingContactForm = (props: IPropyKeysHomeListingContactForm
 
   const classes = useStyles();
 
-  const [recaptchaToken, setRecaptchaToken] = useState("");
   const [hasTriedSubmit, setHasTriedSubmit] = useState(false);
 
   const { executeRecaptcha } = useGoogleReCaptcha();
-
-  // Create an event handler so you can call the verification on button click event or form submit
-  const handleReCaptchaVerify = useCallback(async () => {
-    if (!executeRecaptcha) {
-      console.log('Execute recaptcha not yet available');
-      return;
-    }
-
-    const token = await executeRecaptcha(recaptchaActionKey);
-    setRecaptchaToken(token);
-    // Do whatever you want with the token
-  }, [executeRecaptcha]);
-
-  // You can use useEffect to trigger the verification as soon as the component being loaded
-  useEffect(() => {
-    handleReCaptchaVerify();
-  }, [handleReCaptchaVerify]);
 
   const ValidationSchema = yup.object().shape({
     name: yup.string()
